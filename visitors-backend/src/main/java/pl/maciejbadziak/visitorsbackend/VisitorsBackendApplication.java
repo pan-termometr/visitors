20fbf171310c1b4b60e8e39e9670b54e5f7a12af
pan-termometr <termomemtr@gmail.com>
2023-05-11 21:04:16 +0200

    initial commit

---
+++ visitors-backend/src/main/java/pl/maciejbadziak/visitorsbackend/VisitorsBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.maciejbadziak.visitorsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitorsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitorsBackendApplication.class, args);
+	}
+
+}
---
+++ visitors-backend/src/test/java/pl/maciejbadziak/visitorsbackend/VisitorsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.maciejbadziak.visitorsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitorsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
